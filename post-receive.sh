c31c1914f9983b1056811cbd375916354ab0a50a
all-is-to-be-dared <70043540+all-is-to-be-dared@users.noreply.github.com>
2026-01-13 04:50:04 +0000

    Fix determination of current commit hash in post-receive

--- post-receive.sh
+++ post-receive.sh
@@ -2,19 +2,19 @@
 
 path=$(pwd)
 repo_path="${path}/.."
-socket_path="${path}/../../gradecope-sockets/gradecope-submit.sock"
-
+socket_path="${path}/../../gradecope-sockets/submit.sock"
 
 user=$(whoami)
-commit=$(git rev-parse HEAD)
+
+read oldrev newrev refname
+commit=$(git rev-parse $refname)
+echo "> gradecope: Received branch ${commit}"
 
 submit-job() {
   echo "{\"user\":\"${user}\", \"commit\":\"${commit}\", \"spec\":\"${1}\"}" | socat - UNIX-CONNECT:${socket_path}
 }
 
 let option_count_minus_one=$(($GIT_PUSH_OPTION_COUNT-1))
-
-echo "$option_count_minus_one"
 
 for i in $(seq 0 $option_count_minus_one) ; do
   as_gitvar="GIT_PUSH_OPTION_${i}"
