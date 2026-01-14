0e5de15f6de27f55f35f7c8e55ee03a3e2887e96
Joseph Shetaye <joseph@shetaye.me>
2026-01-13 16:54:45 -0800

    Flesh out ctl interface

--- gradecope-proto/src/lib.rs
+++ gradecope-proto/src/lib.rs
@@ -62,28 +62,72 @@ pub mod runner {
 
 pub mod ctl {
     use tarpc::context;
+    use serde::{Deserialize, Serialize};
+    use thiserror::Error;
 
-    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    #[derive(Debug, Clone, Deserialize, Serialize, Error)]
+    pub enum CtlError {
+	/// Permission denied. Should only be returned when trying to use admin
+	/// commands, not when accessing a resource (prevents information
+	/// leakage)
+	#[error("permission denied")]
+	PermissionDenied,
+	#[error("not found: {0}")]
+	NotFound(String),
+	#[error("internal error: {0}")]
+	InternalError(String)
+    }
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
     pub struct Submission {
         pub commit: String,
         pub spec: String,
     }
 
-    pub struct History {
-
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    pub struct JobReference {
+	pub job_spec: String,
+	pub job_id: uuid::Uuid;
     }
 
-    /// Retrieve 
-    pub struct Status {
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    pub struct Log {
+        pub log: Vec<u8>,
+        pub truncated: bool,
     }
-    
-    #[derive(Debug, serde::Deserialize, serde::Serialize)]
-    pub enum Request {
-        Submission(Submission)
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    pub enum JobResult {
+        Correct,
+        Incorrect,
+        Error,
+        Canceled,
+        Timeout,
+    }
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    pub struct JobStatus {
+	pub job_spec: String,
+	pub job_id: uuid::Uuid;
+	pub result: JobResult;
+    }
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    pub struct Log {
+        pub log: Vec<u8>,
+        pub truncated: bool,
     }
 
     #[tarpc::service]
     pub trait Ctl {
 	async fn hi() -> String;
+	async fn submit(commit: String, job_spec: String) -> Result<(), CtlError>;
+	/// Return job history
+	///
+	/// If given a job spec, returns all jobs for that job spec. Otherwise,
+	/// returns most recent job for each job spec
+	async fn history(job_spec: Option<String>) -> Result<Vec<JobStatus>, CtlError>;
+	async fn status(job: JobReference) -> Result<JobStatus, CtlError>;
+	async fn log(job: JobReference) -> Result<Log, CtlError>;
     } 
 }
